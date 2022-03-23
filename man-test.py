7d45855652675f01e18d775976046dd0e1d5a7a8
ishak <ishak.kozdemir@gmail.com>
2022-03-23 10:54:34 +0300

    manliness-test was aded

---
+++ man-test.py
@@ -0,0 +1,13 @@
+manliness_test = 'give debts', 'greet', 'man don't sell'
+
+manliness = input("enter a word for manliness test:")
+
+if manliness in manliness_test :
+     if manliness == "debt":
+         print("the man's bottom")
+     elif manliness == "hi":
+         print("the one who greets is called a man")
+     else: # manliness == manliness :
+         print("the man is called that")
+else:
+     print("doesn't work")
